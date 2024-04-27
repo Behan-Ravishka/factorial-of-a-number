309799d79ec8983f9321cb9eac47051869987922
M P Behan Ravishka Perera <165493221+Behan-Ravishka@users.noreply.github.com>
2024-04-27 14:58:48 +0530

    modify main.cpp
    modify the code

--- main.cpp
+++ main.cpp
@@ -4,14 +4,25 @@ using namespace std;
 
 int main()
 {
+    //make variables and get input from user
     double number=0,fact=1;
     cout << "Enter a positive integer You want to find Factorial : ";
     cin >> number;
-    for ( ; number>0 ; number-- )
+    //add a condition to check input is postive or negative
+    if (number>=0)
     {
-        fact=number*fact;
+        //if input is positive
+        for ( ; number>0 ; number-- )
+        {
+            fact=number*fact;
+        }
+            cout << "\n\tFactorial of you Entered Number is = " << fact << endl;
     }
-        cout << "\n\tFactorial of you Entered Number is = " << fact << endl;
+    else
+    {
+        //if input is a negative
+        cout << "\n\tInvalid Input! Please enter a Positive Number!" << endl;
+    }
 
     return 0;
 }
